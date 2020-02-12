892a3ac3bbcd6e35389d13d55b685672f367e5ac
NCONTED <NCONTED@mynorth.mymdc.net>
2020-02-12 15:24:10 -0500

    im done yeet

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,13 +3,22 @@ import React, { useState } from 'react'
 const Todos = () => {
     const [todos, setTodos] = useState([])
     const [inputValue, setInputValue] = useState('');
+    const deleteTodos = indexToDelete => {
+        console.log(indexToDelete);
+        setTodos(prevTodos => {
+            return prevTodos.filter((value, index) => {
+                return indexToDelete !== index;
+            })
+        })
+    }
     return (
-        <div className = "YOLO">
-            <h1> My To-Do List</h1>
-            <div className="Zay input-group mb-3">
+        <div className="YOLO">
+            <h1 className="Type">My To-Do List</h1>
+            
+            <div className="Zay input-group">
                 <input type="text" className="form-control" name={inputValue} value={inputValue} onChange={e => setInputValue(e.target.value)} placeholder="Enter New To-Do" aria-label="Enter New To-Do" aria-describedby="button-addon2" />
                 <div className="input-group-append">
-                    <button className="Abigail btn btn-secondary" type="button" onClick={() => {
+                    <button className="btn btn-secondary" type="button" onClick={() => {
                         setTodos(prevTodos => [
                             ...prevTodos, inputValue
                         ])
@@ -19,11 +28,20 @@ const Todos = () => {
                     > Add</button>
                 </div>
             </div>
-            {
-                todos.map((value, index) => {
-                    console.log(value, index);
-                })
-            }
+
+            <ul className="list-group">
+                {todos.map((value, index) => {
+                    return <li className="Abby list-group-item d-flex justify-content-between align-items-center" key={index}>{value}
+                        <span type="button" onClick={() => {
+                            deleteTodos(index);
+                        }}
+                        >x</span>
+                    </li>
+                })}
+                <li className="pain list-group-item d-flex justify-content-between align-items-center">
+                    {todos.length} items
+                 </li>
+            </ul>
         </div>
     )
 }
